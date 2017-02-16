4d512f361c6bb132136b5cf576df5b4d867ab8ee
aldbender <aldbender@gmail.com>
2017-02-15 22:06:38 -0600

    Add files via upload

--- qtl_analysis/master_qtl_plot.R
+++ qtl_analysis/master_qtl_plot.R
@@ -2,7 +2,7 @@ library(ggplot2)
 
 data <- read.delim("/master_qtl_plot.txt", header=TRUE)
 
-data$trait <- factor(data$trait, levels=c("mindex","pct_iso","pct_anteiso","nalk_ACL","iso_ACL","anteiso_ACL","nalk_CPI","iso_CPI","anteiso_CPI","eps13C_n.C31","eps13C_i.C31","eps13C_n.C33","eps13C_i.C33","n31.i31","n33.i33"))
+data$trait <- factor(data$trait, levels=c("mindex","pct_iso","pct_anteiso","nalk_ACL","iso_ACL","anteiso_ACL","nalk_CPI","iso_CPI","anteiso_CPI","D_iC31","D_nC31","D_iC33","D_nC33","n31.i31","n33.i33"))
 
 data$log10_pval <- as.numeric(ifelse(data$p_value>0.05, "NA", -log(data$p_value, base=10)))
 
